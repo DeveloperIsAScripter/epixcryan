7a2ab15f4c8de23c803ffabc119e25a3f562024d
hellamane <82798562+hellamane@users.noreply.github.com>
2022-09-19 23:47:02 -0400

    Create H.lua

---
+++ H.lua
@@ -0,0 +1,382 @@
+getgenv().Egg = false;
+
+local DiscordLib =
+    loadstring(game:HttpGet "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/discord")()
+
+local win = DiscordLib:Window("Homeless Simulator In Russia")
+
+local serv = win:Server("Faven.lua", "")
+
+local ma = serv:Channel("Tools")
+
+ma:Button("Kopeck",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(1)
+end)
+
+ma:Seperator()
+
+ma:Button("Grivna",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(2)
+end)
+
+ma:Seperator()
+
+ma:Button("Euro",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(3)
+end)
+
+ma:Seperator()
+
+ma:Button("5 Euro",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(4)
+end)
+
+ma:Seperator()
+
+ma:Button("10 Euro",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(5)
+end)
+
+ma:Seperator()
+
+ma:Button("100 Euro",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(6)
+end)
+
+ma:Seperator()
+
+ma:Button("200 Euro",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(7)
+end)
+
+ma:Seperator()
+
+ma:Button("500 Euro",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(8)
+end)
+
+ma:Seperator()
+
+ma:Button("1k Rubles",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(9)
+end)
+
+ma:Seperator()
+
+ma:Button("100 Dollars",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(10)
+end)
+
+ma:Seperator()
+
+ma:Button("Robux",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(11)
+end)
+
+ma:Seperator()
+
+ma:Button("Tix",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(12)
+end)
+
+ma:Seperator()
+
+ma:Button("Bag Of Tix",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(13)
+end)
+
+ma:Seperator()
+
+ma:Button("Bag Of Robux",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(14)
+end)
+
+ma:Seperator()
+
+ma:Button("Yen",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(15)
+end)
+
+ma:Seperator()
+
+ma:Button("10k Yen",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(16)
+end)
+
+ma:Seperator()
+
+ma:Button("Chest",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(17)
+end)
+
+ma:Seperator()
+
+ma:Button("Bronze Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(18)
+end)
+
+ma:Seperator()
+
+ma:Button("Silver Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(19)
+end)
+
+ma:Seperator()
+
+ma:Button("Lapis Lazuli Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(20)
+end)
+
+ma:Seperator()
+
+ma:Button("Iron Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(21)
+end)
+
+ma:Seperator()
+
+ma:Button("Gold Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(22)
+end)
+
+ma:Seperator()
+
+ma:Button("Laptop",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(23)
+end)
+
+ma:Seperator()
+
+ma:Button("Diamond Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(24)
+end)
+
+ma:Seperator()
+
+ma:Button("Radio",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(25)
+end)
+
+ma:Seperator()
+
+ma:Button("Ruby Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(26)
+end)
+
+ma:Seperator()
+
+ma:Button("Emerald Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(27)
+end)
+
+ma:Seperator()
+
+ma:Button("Amethyst Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(28)
+end)
+
+ma:Seperator()
+
+ma:Button("Rainbow Ingot",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(29)
+end)
+
+ma:Seperator()
+
+ma:Button("Garbage Bag",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(30)
+end)
+
+ma:Seperator()
+
+ma:Button("Camera",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(31)
+end)
+
+ma:Seperator()
+
+ma:Button("Garbage Can",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(32)
+end)
+
+ma:Seperator()
+
+ma:Button("Red Brilliant",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(33)
+end)
+
+ma:Seperator()
+
+ma:Button("Pop-It",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(34)
+end)
+
+ma:Seperator()
+
+ma:Button("Bitcoin",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(35)
+end)
+
+ma:Seperator()
+
+ma:Button("Iphone",function()
+    game:GetService("ReplicatedStorage").EquippedA:FireServer(36)
+end)
+
+ma:Seperator()
+
+local moon = serv:Channel("Eggs")
+
+moon:Label("<Make Sure You Are In Range With It>")
+
+moon:Seperator()
+
+moon:Toggle("Open Cool Egg",false,function(bool)
+        getgenv().Egg = bool
+    print('Open Cool Egg is: ', bool);
+    if bool then
+        doEgg();
+    end
+    end
+)
+
+moon:Seperator()
+
+moon:Button("Common Egg",function()
+    local args = {
+    [1] = "Common Egg",
+    [2] = "Buy1"}
+game:GetService("ReplicatedStorage").GameClient.Events.RemoteFunction.BuyEgg:InvokeServer(unpack(args))
+end)
+
+moon:Seperator()
+
+moon:Button("E Egg",function()
+    local args = {
+    [1] = "E Egg",
+    [2] = "Buy1"}
+game:GetService("ReplicatedStorage").GameClient.Events.RemoteFunction.BuyEgg:InvokeServer(unpack(args))
+end)
+
+moon:Seperator()
+
+moon:Button("Epic Egg",function()
+    local args = {
+    [1] = "Epic Egg",
+    [2] = "Buy1"}
+game:GetService("ReplicatedStorage").GameClient.Events.RemoteFunction.BuyEgg:InvokeServer(unpack(args))
+end)
+
+moon:Seperator()
+
+moon:Button("Chroma Egg",function()
+    local args = {
+    [1] = "Chroma Egg",
+    [2] = "Buy1"}
+game:GetService("ReplicatedStorage").GameClient.Events.RemoteFunction.BuyEgg:InvokeServer(unpack(args))
+end)
+
+moon:Seperator()
+
+moon:Button("Cool Egg",function()
+    local args = {
+    [1] = "Cool Egg",
+    [2] = "Buy1"}
+game:GetService("ReplicatedStorage").GameClient.Events.RemoteFunction.BuyEgg:InvokeServer(unpack(args))
+end)
+
+moon:Seperator()
+
+local z = serv:Channel("Misc")
+	
+z:Button("Low Graphics", function()
+			
+			local g = game
+local w = g.Workspace
+local l = g.Lighting
+local t = w.Terrain
+t.WaterWaveSize = 0
+t.WaterWaveSpeed = 0
+t.WaterReflectance = 0
+t.WaterTransparency = 0
+l.GlobalShadows = false
+l.FogEnd = 9e9
+l.Brightness = 0
+settings().Rendering.QualityLevel = "Level01"
+for i, v in pairs(g:GetDescendants()) do
+    if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+    elseif v:IsA("Decal") or v:IsA("Texture") and decalsyeeted then
+        v.Transparency = 1
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+        v.Lifetime = NumberRange.new(0)
+    elseif v:IsA("Explosion") then
+        v.BlastPressure = 1
+        v.BlastRadius = 1
+    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+        v.Enabled = false
+    elseif v:IsA("MeshPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+        v.TextureID = 10385902758728957
+    end
+end
+end)
+	
+z:Seperator()
+
+z:Button("Infinite Jump", function()
+    
+    local InfiniteJumpEnabled = true
+game:GetService("UserInputService").JumpRequest:connect(function()
+	if InfiniteJumpEnabled then
+		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+	end
+end)
+    
+end)
+
+z:Seperator()
+
+z:Button("Fire Touch Interest", function()
+               for _, v in pairs(game.Workspace:GetDescendants()) do
+                 if v:IsA("TouchTransmitter") then
+                     firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart or game.Players.LocalPlayer.Character.Torso, v:FindFirstAncestorWhichIsA("Part"),0)
+                 end
+               end
+end)
+
+
+z:Seperator()
+
+z:Button("Remove Void", function()
+    
+workspace.FallenPartsDestroyHeight = 0 / 0
+    
+end)
+
+z:Seperator()
+
+local cred = serv:Channel("Credits")
+
+cred:Label("Made by hviqz#0001")
+
+cred:Seperator()
+
+cred:Label("UI by dawid#7205")
+
+cred:Seperator()
+
+function doEgg()
+    spawn(function()
+        while getgenv().Egg == true do
+                wait(0.040)
+                local args = {
+                    [1] = "Cool Egg",
+                    [2] = "Buy1"}
+                game:GetService("ReplicatedStorage").GameClient.Events.RemoteFunction.BuyEgg:InvokeServer(unpack(args))
+                wait(.1)
+        end
+    end)
+end
